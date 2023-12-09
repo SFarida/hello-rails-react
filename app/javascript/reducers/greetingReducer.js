70e42afe0383af8a6b2364703e8a0a3b5c6e757c
SFarida <faridagueleu@gmail.com>
2023-12-09 12:10:47 +0100

    add api

---
+++ app/javascript/reducers/greetingReducer.js
@@ -0,0 +1,12 @@
+// app/javascript/reducers/greetingReducer.js
+
+const greetingReducer = (state = '', action) => {
+    switch (action.type) {
+        case 'SET_GREETING':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+export default greetingReducer;
